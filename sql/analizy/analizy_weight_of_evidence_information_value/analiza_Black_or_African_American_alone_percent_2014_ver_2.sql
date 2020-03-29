7aee349d1b9a99fcce27df44acaff2ea576e5960
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2020-03-29 18:34:45 +0200

    final fixes

--- sql/analizy/analizy_weight_of_evidence_information_value/analiza_Black_or_African_American_alone_percent_2014_ver_2.sql
+++ sql/analizy/analizy_weight_of_evidence_information_value/analiza_Black_or_African_American_alone_percent_2014_ver_2.sql
@@ -7,11 +7,11 @@ from county_facts;
 
 -- Analiza WOE & IV
 
-with a as
+with k1 as
          (
              select r.fips,
                     case
-                        when f.RHI225214 < 20 then '1) niski udzia³ czarnoskorych'
+                        when f.RHI225214 < 1 then '1) niski udzia³ czarnoskorych'
                         when f.RHI225214 < 40 then '2) sredni udzia³ czarnoskorych'
                         else '3) wysoki udzia³ czarnoskorych'
                         end as     kategoria,
@@ -19,25 +19,6 @@ with a as
                         when sum(case when r.party like 'Repub%' then r.votes end) >
                              sum(case when r.party like 'Democ%' then r.votes end) then 1
                         else 0 end republikanie,
-                    0              demokraci
-             from primary_results r
-                      join county_facts f on r.fips = f.fips
-             group by r.fips, kategoria
-             having (case
-                         when sum(case when r.party like 'Repub%' then r.votes end) >
-                              sum(case when r.party like 'Democ%' then r.votes end) then 1
-                         else 0 end) = 1
-             limit 1050
-         ),
-     b as
-         (
-             select r.fips,
-                    case
-                        when f.RHI225214 < 20 then '1) niski udzia³ czarnoskorych'
-                        when f.RHI225214 < 40 then '2) sredni udzia³ czarnoskorych'
-                        else '3) wysoki udzia³ czarnoskorych'
-                        end as     kategoria,
-                    0              republikanie,
                     case
                         when sum(case when r.party like 'Repub%' then r.votes end) <
                              sum(case when r.party like 'Democ%' then r.votes end) then 1
@@ -45,18 +26,6 @@ with a as
              from primary_results r
                       join county_facts f on r.fips = f.fips
              group by r.fips, kategoria
-             having (case
-                         when sum(case when r.party like 'Repub%' then r.votes end) <
-                              sum(case when r.party like 'Democ%' then r.votes end) then 1
-                         else 0 end) = 1
-         ),
-     k1 as
-         (
-             select *
-             from a
-             union
-             select *
-             from b
          ),
      k2 as
          (
@@ -82,7 +51,10 @@ with a as
      k5 as
              (select *, woe * dr_minus_dd iv from k4)
 select *, sum(iv) over () suma_iv
-from k5;
+from k5
+
+
+
 
 
 
